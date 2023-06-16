5ec5cb2c928ed81ba630f148d80cc1d01a628409
Akshay Mundra <mundraak2704@gmail.com>
2023-06-16 22:03:10 +0530

    added header for probehave

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Preferences from "./components/content/preferences/Preferences";
 import Notifications from "./components/content/notifications/Notification";
 import SecuritySetting from "./components/content/security_setting/SecuritySetting";
 import Sidebar from "./components/sidebar/Sidebar";
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import 'antd/dist/reset.css';
 
 import Subscription from "./components/content/subscription/Subscription";
@@ -31,64 +31,66 @@ import SignupSuccess from "./components/authenticate/signup/signup_success/Signu
 // import Card from "./components/probehave/module_card/Card";
 // import Popup from "./components/probehave/welcome_popup/Popup";
 // import Checkout from "./components/probehave/checkout/Checkout";
+import Header from "./components/probehave/header/Header";
 
 const App = () => {
-  return ( 
+  return (
     <Router>
-    <div className="main">
-      {/* <div className="side-container">
+      <div className="main">
+        {/* <div className="side-container">
         <Sidebar />
       </div> */}
-      <div className="content-container">
-        <div className="">
-          <Routes>
-            <Route path="/" element={<Notifications />} />
+        <div className="content-container">
+          <div className="">
+            <Routes>
+              <Route path="/" element={<Notifications />} />
 
-            {/* authentication */}
-            <Route path="/signin" element={<Signin />} />
-            <Route path="/verify-otp" element={<TwoFAOtp />} />
-            <Route path="signup" element={<Signup />} />
-            <Route path="signup-two" element={<SignupTwo />} />
-            <Route path="signup-success" element={<SignupSuccess />} />
+              {/* authentication */}
+              <Route path="/signin" element={<Signin />} />
+              <Route path="/verify-otp" element={<TwoFAOtp />} />
+              <Route path="signup" element={<Signup />} />
+              <Route path="signup-two" element={<SignupTwo />} />
+              <Route path="signup-success" element={<SignupSuccess />} />
 
 
-            
 
-            {/* shipper card  */}
-            {/* <Route path="/shipper1" element={<Shipper />} /> */}
-            <Route path="/shipper" element={<Shipper />} />
 
-            {/* forwarder  */}
-            <Route path="/forwarder-dashboard" element={<Dashboard />} />
-            <Route path="/forwarder-documents" element={<Documents />} />
-            <Route path="/forwarder-custom-quotes" element={<CustomQuotes />} />
+              {/* shipper card  */}
+              {/* <Route path="/shipper1" element={<Shipper />} /> */}
+              <Route path="/shipper" element={<Shipper />} />
 
-            {/* settings section  */}
-            {/* <Route path="/settings/account" element={<Account />} />
+              {/* forwarder  */}
+              <Route path="/forwarder-dashboard" element={<Dashboard />} />
+              <Route path="/forwarder-documents" element={<Documents />} />
+              <Route path="/forwarder-custom-quotes" element={<CustomQuotes />} />
+
+              {/* settings section  */}
+              {/* <Route path="/settings/account" element={<Account />} />
             <Route path="/settings/company" element={<Company />} />
             <Route path="/settings/subscription" element={<Subscription />} />
             <Route path="/settings/security-setting" element={<SecuritySetting />} />
             <Route path="/settings/referrals" element={<Referrals />} />
             <Route path="/settings/notifications" element={<Notifications />} />
-            <Route path="/settings/preferences" element={<Preferences />} /> */}
+          <Route path="/settings/preferences" element={<Preferences />} /> */}
 
 
-            {/* probehave --------------------------
+              {/* probehave --------------------------
             <Route path="/probehave-profile" element={<Profile />} />
             <Route path="/probehave-modulecard" element={<Card />} />
             <Route path="/probehave-popup" element={<Popup />} />
             <Route path="/probehave-checkout" element={<Checkout />} /> */}
+              <Route path="/probehave-header" element={<Header />} />
 
-            {/* not in use */}
-            {/* drawer  */}
-            <Route path="/drawer" element={<DrawerHome />} />
-          </Routes>
+              {/* not in use */}
+              {/* drawer  */}
+              <Route path="/drawer" element={<DrawerHome />} />
+            </Routes>
 
+          </div>
         </div>
       </div>
-    </div>
     </Router>
-   );
+  );
 }
- 
+
 export default App;
---
+++ src/components/probehave/header/Header.jsx
@@ -0,0 +1,125 @@
+import Style from "./header.module.css";
+import { Dropdown, Button } from "antd";
+import { DownOutlined, MenuOutlined } from "@ant-design/icons";
+
+const items = [
+    {
+        label: (
+            <a target="_blank" rel="noopener noreferrer" href="#">
+                item 1
+            </a>
+        ),
+        key: '0',
+    },
+    {
+        label: (
+            <a target="_blank" rel="noopener noreferrer" href="#">
+                item 2
+            </a>
+        ),
+        key: '1',
+    }
+]
+
+const Header = () => {
+
+    const toggleDropdown = () => {
+        const dropdown = document.getElementById("dropdown");
+        dropdown.classList.toggle(Style.hide_dropdown)
+    }
+
+    return (
+        <div className={`${Style.nav_bar}`}>
+
+            <div className={`${Style.right_container}`}>
+
+                <div className={`${Style.brand_container}`}>
+                    {/* logo here */}
+                    <img src="#" style={{ width: '30px', height: '30px' }} />
+                    <h2 style={{ margin: '0' }}>Probehave</h2>
+                </div>
+
+                {/* desktop nav  */}
+                <div className={`${Style.nav_links} ${Style.desktop_header}`}>
+                    <a href="#"><span className={Style.nav_link}>Home</span></a>
+
+                    <Dropdown
+                        menu={{
+                            items,
+                        }}
+                    >
+                        <a onClick={(e) => e.preventDefault()} style={{ cursor: 'pointer' }}>
+                            <div className={`${Style.nav_dropdown}`} >
+                                <span className="font-light font-title">Products</span>
+                                <DownOutlined style={{ color: 'gray', fontSize: '10px' }} />
+                            </div>
+                        </a>
+                    </Dropdown>
+                    <Dropdown
+                        menu={{
+                            items,
+                        }}
+                    >
+                        <a onClick={(e) => e.preventDefault()} style={{ cursor: 'pointer' }}>
+                            <div className={`${Style.nav_dropdown}`} >
+                                <span className={Style.nav_link}>Resources</span>
+                                <DownOutlined style={{ color: 'gray', fontSize: '10px' }} />
+                            </div>
+                        </a>
+                    </Dropdown>
+
+
+                    <a href="#"><span className={Style.nav_link}>Pricing</span></a>
+                </div>
+            </div>
+
+            {/* user actions  */}
+            <div className={`${Style.desktop_header} ${Style.user_actions}`}>
+                <Button className={`${Style.login_btn}`}>
+                    Log in
+                </Button>
+                <Button className={`${Style.signup_btn}`}>
+                    Sign up
+                </Button>
+            </div>
+
+            <div className={`${Style.mobile_header}`}>
+                <Button
+                    style={{ border: 'none', boxShadow: 'none' }}
+                    onClick={toggleDropdown}
+                ><MenuOutlined /></Button>
+
+                <div id="dropdown" className={`${Style.dropdown} ${Style.hide_dropdown}`}>
+                    <a href="#"><span className={`${Style.nav_link}`}>Home</span></a>
+                    <Dropdown
+                        menu={{ items, }}
+                    >
+                        <a onClick={(e) => e.preventDefault()} style={{ cursor: 'pointer' }}>
+                            <div className={`${Style.nav_dropdown}`} >
+                                <span className={`${Style.nav_link}`}>Products</span>
+                                <DownOutlined style={{ color: 'gray', fontSize: '10px' }} />
+                            </div>
+                        </a>
+                    </Dropdown>
+                    <Dropdown
+                        menu={{ items, }}
+                    >
+                        <a onClick={(e) => e.preventDefault()} style={{ cursor: 'pointer' }}>
+                            <div className={`${Style.nav_dropdown}`} >
+                                <span className={`${Style.nav_link}`}>Resources</span>
+                                <DownOutlined style={{ color: 'gray', fontSize: '10px' }} />
+                            </div>
+                        </a>
+                    </Dropdown>
+                    <a href="#"><span className={`${Style.nav_link}`}>Pricing</span></a>
+                </div>
+            </div>
+
+
+
+
+        </div>
+    );
+}
+
+export default Header;
